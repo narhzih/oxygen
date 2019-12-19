193caf4a5d809ff913b07e2d824e8744d598c194
Phantom <symfony500@gmail.com>
2019-12-19 08:41:50 +0100

    Added framework Backbone

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,16 @@
 <?php require_once '../app/views/inc/header.php' ?>
 
-
-
+    <div class="jumbotron">
+        <h1 class="display-4 text-center">Oxygen Framework</h1>
+        <p class="lead text-center">A simple PHP FRAMEWORK Built with PHP</p>
+        <hr class="my-4 text-center">
+        <p class="text-center">Please read the docs to know more about the framework and how to get a copy of it.
+            Development is still going on and contributions are allowed at the moment. Below is the link
+            to the github repository for the framework
+        </p>
+        <p class="lead text-center">
+            <a class="btn btn-dark btn-lg" href="#" role="button">Visit Github</a>
+        </p>
+        <h6 class="text-center">Author: Omosekeji Olawale</h6>
+    </div>
 <?php require_once '../app/views/inc/footer.php' ?>

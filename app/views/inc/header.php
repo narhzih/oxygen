ce2deec02937e711a99258d5a3bfe1f8cb415178
Phantom <symfony500@gmail.com>
2019-12-19 08:38:33 +0100

    Added framework stylesheet

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -21,7 +21,7 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="<?php echo URLROOT . '/pages/index' ?>">
                         Home <span class="sr-only">(current)</span>
@@ -33,15 +33,7 @@
 
 
             </ul>
-            <ul class="navbar-nav ml-auto">
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT . '/users/login' ?>">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URLROOT . '/users/register' ?>">Register</a>
-                </li>
-            </ul>
+
         </div>
     </div>
 </nav>

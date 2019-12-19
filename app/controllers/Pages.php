77ccc6a184948b75b7e5872fa1b211b618a89a8e
Phantom <symfony500@gmail.com>
2019-12-19 08:32:19 +0100

    Convertin to core framework

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,28 +2,22 @@
 
 class Pages extends Controller
 {
-    private $postModel;
 
     public function __construct()
     {
-        $this->postModel = $this->model('Posts');
+
     }
 
     public function index()
     {
-        $posts = $this->postModel->getPosts();
+
         $data = [
             'title' => 'Welcome',
-            'posts' => $posts
         ];
         $this->view('pages/index', $data);
     }
 
     public function about()
     {
-        $data = [
-            'title' => 'Welcome'
-        ];
-        $this->view('pages/about', $data);
     }
 }

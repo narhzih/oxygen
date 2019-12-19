2e91647ac4d73d1092d786abc40823f37eb590a3
Phantom <symfony500@gmail.com>
2019-12-19 08:33:41 +0100

    Added site header

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,7 +6,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= SITENAME; ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= URLROOT ?>/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 
 <body>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark ">
+    <div class="container">
+        <a class="navbar-brand" href="#">OxyBlogger</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="<?php echo URLROOT . '/pages/index' ?>">
+                        Home <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+
+
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT . '/users/login' ?>">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT . '/users/register' ?>">Register</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container">

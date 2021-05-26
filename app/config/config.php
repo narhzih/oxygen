57b3c248252a1a1f6a20f0e898fac59005a1a75d
Symfony <omosekejiolawale@gmail.com>
2021-05-26 03:55:34 +0100

    Converted defines to const declaration in config file

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,12 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', '_YOUR_USERNAME_HERE');
-define('DB_PWD', '_YOUR_PASSWORD_HERE');
-define('DB_NAME', '_YOUR_DBNAME_HERE');
+const DB_HOST = 'localhost';
+const DB_USER = '_YOUR_USERNAME_HERE';
+const DB_PWD = '_YOUR_PASSWORD_HERE';
+const DB_NAME = '_YOUR_DBNAME_HERE';
 
 
 
 //Define application information
-define('APPROOT', dirname(dirname(__FILE__)));
-
-define('URLROOT', 'http://localhost/LMVC');
-
-define('SITENAME', 'KonsoleMVC');
+define('APP_ROOT', dirname(dirname(__FILE__)));
+const URL_ROOT = 'http://localhost/LMVC';
+const SITE_NAME = 'Oxygen MVC';
